15d03eccfafd8db25cec5eda2c10024413ae99c8
profesor <profesor@Patata-2>
2020-10-27 17:52:00 +0100

    Commit inicial

---
+++ src/tutoria_27_10_20/Tutoria_27_10_20.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutoria_27_10_20;
+
+/**
+ *
+ * @author profesor
+ */
+public class Tutoria_27_10_20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
